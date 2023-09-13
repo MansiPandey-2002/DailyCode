30d251c37732c3a38047bed03ca8bcfd51bd8eef
Mansi Pandey <125549121+MansiPandey-2002@users.noreply.github.com>
2023-09-13 14:35:04 +0000

    Update largestElement.cpp

--- Arrays/Easy/largestElement.cpp
+++ Arrays/Easy/largestElement.cpp
@@ -1,1 +1,10 @@
-
+#include <bits/stdc++.h> 
+int largestElement(vector<int> &arr, int n) {
+    int max=arr[0];
+    for(auto i:arr)
+    {
+        if(i>max)
+            max=i;
+    }
+    return max;
+}
