9406b570523c06992fe66f5479d8b6647630546d
Mansi Pandey <125549121+MansiPandey-2002@users.noreply.github.com>
2023-09-15 18:00:36 +0000

    Update alternateNumbers.cpp

--- Arrays/Medium/alternateNumbers.cpp
+++ Arrays/Medium/alternateNumbers.cpp
@@ -16,3 +16,24 @@ vector<int> alternateNumbers(vector<int>&a) {
     }
     return a;
 }
+
+//OPTIMAL
+vector<int> alternateNumbers(vector<int>&a) {
+    int pos=0,neg=1;
+    int n=a.size();
+    vector<int> ans(n,0);
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]>=0)
+        {
+            ans[pos]=a[i];
+            pos+=2;
+        }
+        else
+        {
+            ans[neg]=a[i];
+            neg+=2;
+        }
+    }
+    return ans;
+}
