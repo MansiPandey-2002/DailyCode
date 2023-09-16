f204d58b907cc1b14b072a65197babd1232ff36a
Mansi Pandey <125549121+MansiPandey-2002@users.noreply.github.com>
2023-09-16 18:21:41 +0000

    Update read.cpp

--- Arrays/Medium/read.cpp
+++ Arrays/Medium/read.cpp
@@ -11,3 +11,22 @@ string read(int n, vector<int> book, int target)
     }
     return "NO";
 }
+
+
+//optimal solution
+string read(int n, vector<int> book, int target)
+{
+   sort(book.begin(),book.end());
+   int l=0;
+   int r=n-1;
+   for(int i=0;i<n;i++)
+   {
+        if(book[l]+book[r]==target)
+            return "YES";
+        else if(book[l]+book[r]>target)
+            r--;
+        else
+            l++;
+   }
+   return "NO";
+}
